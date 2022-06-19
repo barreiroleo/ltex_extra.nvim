3abd364797ba64cc8fe880becc7638511d86157d
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-06-18 21:54:24 -0300

    feat: commands called by Command Handler.

--- ltex.lua
+++ ltex.lua
@@ -1,27 +1,17 @@
 local inspect = require("inspect")
-local lnsep = "\n==========\n"
-local function println(arg) print(lnsep .. arg .. lnsep) end
+local lnsep = "=========="
+local function println(arg) print("\n" .. lnsep .. arg .. lnsep) end
 
 local commands = require("commands")
 
--- Command Handler mockup.
-local function execute_command(command)
-    println(inspect(command))
-    if command.command == '_ltex.addToDictionary' then
-        println("addToDictionary triggered")
-    end
-end
-
 local function addToDictionary(command)
     local args = command.arguments[1].words
-    println(inspect(args))
     for lang, words in pairs(args) do
-        println("Lang: " .. inspect(lang) .. "\n" ..
-                "Words: " .. inspect(words))
+        print("Lang: " .. inspect(lang) .. "\n" ..
+            "Words: " .. inspect(words))
         local file = io.open("ltex.dictionary." .. lang .. ".txt", "a+")
         io.output(file)
         for _, word in ipairs(words) do
-            print(word)
             io.write(word .. "\n")
         end
         io.close(file)
@@ -30,14 +20,12 @@ end
 
 local function disableRules(command)
     local args = command.arguments[1].ruleIds
-    println(inspect(args))
     for lang, rules in pairs(args) do
-        println("Lang: " .. inspect(lang) .. "\n" ..
-                "Rules: " .. inspect(rules))
+        print("Lang: " .. inspect(lang) .. "\n" ..
+            "Rules: " .. inspect(rules))
         local file = io.open("ltex.disabledRules." .. lang .. ".txt", "a+")
         io.output(file)
         for _, rule in ipairs(rules) do
-            print(rule)
             io.write(rule .. "\n")
         end
         io.close(file)
@@ -46,21 +34,31 @@ end
 
 local function hideFalsePositives(command)
     local args = command.arguments[1].falsePositives
-    println(inspect(args))
     for lang, rules in pairs(args) do
-        println("Lang: " .. inspect(lang) .. "\n" ..
-                "Rules: " .. inspect(rules))
+        print("Lang: " .. inspect(lang) .. "\n" ..
+            "Rules: " .. inspect(rules))
         local file = io.open("ltex.hiddenFalsePositives." .. lang .. ".txt", "a+")
         io.output(file)
         for _, rule in ipairs(rules) do
-            print(rule)
             io.write(rule .. "\n")
         end
         io.close(file)
     end
 end
 
--- execute_command(command.dictionary)
-addToDictionary(commands.dictionary)
-disableRules(commands.disableRules)
-hideFalsePositives(commands.hideFalsePositives)
+-- Command Handler mockup.
+local function execute_command(command)
+    println(command.command)
+    print("command = " .. inspect(command))
+    if command.command == '_ltex.addToDictionary' then
+        addToDictionary(commands.dictionary)
+    elseif command.command == '_ltex.disableRules' then
+        disableRules(commands.disableRules)
+    elseif command.command == '_ltex.hideFalsePositives' then
+        hideFalsePositives(commands.hideFalsePositives)
+    end
+end
+
+execute_command(commands.dictionary)
+execute_command(commands.disableRules)
+execute_command(commands.hideFalsePositives)
