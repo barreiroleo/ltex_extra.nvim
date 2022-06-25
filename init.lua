9fcdb9aa7b3e6a5428c54d97c2c565fea20be9af
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-06-25 20:57:17 -0300

    wip: adapted path in requires for working as plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- local pathOfThisFile = ... -- pathOfThisFile is now 'lib.foo.bar'
-local folderPath = (...):match("(.-)[^%.]+$") -- returns 'lib.foo.'
-
-local command_handler = require(folderPath .. "src.commands-lsp")
+-- local folderPath = (...):match("(.-)[^%.]+$") -- returns 'lib.foo.'
+-- local command_handler = require(folderPath .. "src.commands-lsp")
+local command_handler = require("commands-lsp")
 
 return command_handler
