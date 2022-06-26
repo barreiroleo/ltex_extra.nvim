d8c4b74bc9b18cf66ad336a57477787553d5ecdf
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-06-26 04:51:49 -0300

    feat(setup): Configuracions and startup actions.
    Capability off receive configurations and merge with defaults.
    Command handler starts in setup call.
    Capability off load external LTeX files in on_attach event.

--- lua/latex/init.lua
+++ lua/latex/init.lua
@@ -1,6 +1,28 @@
 -- local pathOfThisFile = ... -- pathOfThisFile is now 'lib.foo.bar'
 -- local folderPath = (...):match("(.-)[^%.]+$") -- returns 'lib.foo.'
 -- local command_handler = require(folderPath .. "src.commands-lsp")
-local command_handler = require("latex.src.commands-lsp")
 
-return command_handler
+local M = {}
+
+local default_opts = {
+    load_langs = {"es-AR", "en-US"},
+    init_check = false,
+}
+
+M.opts = {}
+
+M.setup = function (opts)
+    M.opts = opts or default_opts
+    for key, value in pairs(default_opts) do
+        if not M.opts[key] then
+            M.opts[key] = value
+        end
+    end
+
+    vim.lsp.buf.execute_command = require("latex.src.commands-lsp").commandHandler
+    if opts.init_check == true then
+        require("latex.src.commands-lsp").updateConfigFull(opts.load_langs)
+    end
+end
+
+return M
