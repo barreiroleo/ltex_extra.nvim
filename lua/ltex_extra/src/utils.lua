bd8ec095df9adcd72a39bace0f450dbf69f05368
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-07-04 17:23:45 -0300

    feat: Export to path and Reload function.
    Export and load ltex from custom path.
    Reload ltex data like api style.
    Many refactors.

--- lua/ltex_extra/src/utils.lua
+++ lua/ltex_extra/src/utils.lua
@@ -1,16 +1,53 @@
+local log  = require("ltex_extra.src.log")
+local path = package.loaded.ltex_extra.opts.path
+
 local M = {}
 
--- PRIVATE FUNCTIONS
-
-M.file_exists = function(filename)
-    local file = io.open(filename, "r")
-    if file ~= nil then io.close(file) return true else return false end
+-- Returns the filename for a type and lang required.
+M.joinPath = function(type, lang)
+    return path .. table.concat({ "ltex", type, lang, "txt" }, ".")
 end
 
--- PUBLIC FUNCTIONS
+-- Check if path exist. Apply for files and dirs.
+M.path_exist = function(path)
+    log.trace("Checking path ", path)
+    local isok, errstr, errcode = os.rename(path, path)
+    if isok == nil then
+        log.warn(errstr, errcode)
+        return false
+    end
+    return true
+end
 
-M.writeFile = function(type, lang, lines)
-    local filename = table.concat({ "ltex", type, lang, "txt" }, ".")
+-- Making a especified directory and check if created succesfully.
+M.mkdir = function(dirname)
+    log.trace("Making dir ", dirname)
+    os.execute("mkdir " .. dirname)
+    if M.path_exist(dirname) then
+        log.info("Directory making succesfully ", dirname)
+        return true
+    else
+        log.vimwarn("Fail making directory")
+        return false
+    end
+end
+
+-- Check if a dir exist, if not, try to making it.
+M.check_dir = function(dirname)
+    log.trace("Checking dir", dirname)
+    if dirname == "" then return true end
+    if not M.path_exist(dirname) then
+        log.vimwarn(dirname .. " not found, making it")
+        if M.mkdir(dirname) then return true
+        else return false end
+    else
+        return true
+    end
+end
+
+-- Write especified content into a file.
+M.writeFile = function(filename, lines)
+    log.trace("Writing ", filename)
     local file = io.open(filename, "a+")
     io.output(file)
     for _, line in ipairs(lines) do
@@ -19,36 +56,42 @@ M.writeFile = function(type, lang, lines)
     io.close(file)
 end
 
-M.readFile = function(type, lang)
-    local filename = table.concat({ "ltex", type, lang, "txt" }, ".")
-    local lines = {}
-    if M.file_exists(filename) then
-        local file = io.open(filename, "r")
-        io.input(file)
-        for line in io.lines(filename) do
-            lines[#lines + 1] = line
-        end
-        io.close(file)
+-- Export ltex data depends on the type and lang especified.
+M.exportFile = function(type, lang, lines)
+    log.trace("Exporting ", type, lang, lines)
+    local filename = M.joinPath(type, lang)
+    if M.check_dir(path) then
+        M.writeFile(filename, lines)
+    else
+        log.vimwarn("Fail export " .. filename)
     end
+end
+
+-- Return the content of a required file
+M.readFile = function(filename)
+    log.trace("Reading ", filename)
+    local lines, file = {}, io.open(filename, "r")
+    io.input(file)
+    for line in io.lines(filename) do
+        lines[#lines + 1] = line
+    end
+    io.close(file)
     return lines
 end
 
-M.loadFile = function(type, langs)
+-- Return the content of a required type and lang.
+-- If the file doesn't exist, return empty table.
+M.loadFile = function(type, lang)
+    log.trace("Loading ", type, lang)
     local content = {}
-    for _, lang in ipairs(langs) do
-        content[lang] = M.readFile(type, lang)
-    end
-    return content
-end
-
-M.inspect = function(args)
-    local inspect = nil
-    if vim then
-        inspect = vim.inspect
+    local filename = M.joinPath(type, lang)
+    if M.path_exist(filename) then
+        content = M.readFile(filename)
     else
-        inspect = require("inspect") -- from luarocks
+        content = {}
     end
-    return inspect(args)
+    log.debug(vim.inspect(content))
+    return content
 end
 
 return M
