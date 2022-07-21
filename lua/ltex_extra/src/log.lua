bef1f70796d54fde6f4c350f2183fed5c0255c9b
Pierrick Guillaume <pierguill@gmail.com>
2022-07-20 21:31:59 -0400

    ref(debug): allow debug configuration in setup

--- lua/ltex_extra/src/log.lua
+++ lua/ltex_extra/src/log.lua
@@ -1,42 +1,56 @@
-local debug = false
-
 local log = {}
 
-if debug then
-    log = require("plenary.log").new {
-        plugin = "ltex_extra",
-        use_file = false,
-        level = "trace"
-    }
-else
-    local dummy = function (...) return true end
-    log.trace     = dummy
-    log.debug     = dummy
-    log.info      = dummy
-    log.warn      = dummy
-    log.error     = dummy
-    log.fatal     = dummy
-    log.fmt_trace = dummy
-    log.fmt_debug = dummy
-    log.fmt_info  = dummy
-    log.fmt_warn  = dummy
-    log.fmt_error = dummy
-    log.fmt_fatal = dummy
-end
+log.init = function (level)
+    if type(level) == "string" then
+        local tmp_log = require("plenary.log").new {
+            plugin = "ltex_extra",
+            use_file = false,
+            level = level
+        }
+        for k, v in pairs(tmp_log) do
+          log[k] = v
+        end
+    elseif type(level) == "boolean" and level == true then
+        local tmp_log = require("plenary.log").new {
+            plugin = "ltex_extra",
+            use_file = false,
+            level = "warn"
+        }
+        for k, v in pairs(tmp_log) do
+          log[k] = v
+        end
+    else
+        local dummy = function (...) return true end
+        log.trace     = dummy
+        log.debug     = dummy
+        log.info      = dummy
+        log.warn      = dummy
+        log.error     = dummy
+        log.fatal     = dummy
+        log.fmt_trace = dummy
+        log.fmt_debug = dummy
+        log.fmt_info  = dummy
+        log.fmt_warn  = dummy
+        log.fmt_error = dummy
+        log.fmt_fatal = dummy
+    end
 
-log.viminfo = function (...)
-    vim.notify(...)
-    log.info(...)
-end
+    log.viminfo = function (...)
+        vim.notify(...)
+        log.info(...)
+    end
 
-log.vimwarn = function (...)
-    vim.notify(..., vim.log.levels.WARN)
-    log.warn(...)
-end
+    log.vimwarn = function (...)
+        vim.notify(..., vim.log.levels.WARN)
+        log.warn(...)
+    end
 
-log.vimerror = function (...)
-    vim.notify(..., vim.log.levels.ERROR)
-    log.error(...)
+    log.vimerror = function (...)
+        vim.notify(..., vim.log.levels.ERROR)
+        log.error(...)
+    end
+
+    return log
 end
 
 return log
