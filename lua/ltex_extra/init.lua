89c4cc106379933eb08719a4d11f79cfd8732a68
LeoBarreiro <a.leo.barreiro@gmail.com>
2022-07-22 04:30:04 -0300

    feat(logs): Exposing logs options to public settings
    Restructured the way that logs operate.
    The `init(level)` function no longer receives a `level`. Instead, the setup
    reads the settings from package.load. This allows to have the same behavior for
    all the modules that require to use logs.
    
    The `plenary` dependency is handled by a pcall statement. If the user hasn't
    installed `plenary`, be notified and use the built-in logs. This also applies to
    the logging level "none".

--- lua/ltex_extra/init.lua
+++ lua/ltex_extra/init.lua
@@ -4,19 +4,18 @@ local default_opts = {
     load_langs = { "es-AR", "en-US" }, -- table <string> : language for witch dictionaries will be loaded
     init_check = true, -- boolean : whether to load dictionaries on startup
     path = nil, -- string : path to store dictionaries. Relative path uses current working directory
-    log_level = "none", -- string : "none" for no logs or plenary log level. One of "trace", "debug", "info", "warn", "error", "fatal"
+    log_level = "none", -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
 }
 
 M.opts = {}
 
-M.reload = function (...) require("ltex_extra.src.commands-lsp").reload(...) end
+M.reload = function(...) require("ltex_extra.src.commands-lsp").reload(...) end
 
 M.setup = function(opts)
-    log.trace("Merge options")
     if opts.path then opts.path = opts.path .. "/" else opts.path = "" end
     M.opts = vim.tbl_deep_extend('force', default_opts, opts)
 
-    local log = require("ltex_extra.src.log").init(M.opts.log_level)
+    local log = require("ltex_extra.src.log")
     log.debug("Opts: " .. vim.inspect(M.opts))
 
     log.trace("Add commands to lsp")
