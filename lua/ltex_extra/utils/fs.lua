5d639af2cb383d89d5c6df43b070190ac9797c3b
Joshua Manchester <joshuamanchester4@gmail.com>
2023-05-29 15:12:18 +0100

    refactor: use libuv for file operations

--- lua/ltex_extra/utils/fs.lua
+++ lua/ltex_extra/utils/fs.lua
@@ -22,56 +22,58 @@ end
 -- Check if path exist. Apply for files and dirs.
 M.path_exist = function(path)
     log.trace("Checking path ", path)
-    local isok, errstr, errcode = os.rename(path, path)
-    if isok == nil then
-        log.warn(errstr, errcode)
-        return false
-    end
-    return true
+    return vim.loop.fs_stat(path) ~= nil
 end
 
--- Making a especified directory and check if created succesfully.
+-- Make a specified directory and check if created succesfully.
 M.mkdir = function(dirname)
     log.trace("Making dir ", dirname)
-    os.execute("mkdir " .. dirname)
-    if M.path_exist(dirname) then
-        log.info("Directory making succesfully ", dirname)
-        return true
-    else
-        log.vimwarn("Fail making directory")
+    local perm = tonumber("744", 8)
+    local ok, err = vim.loop.fs_mkdir(dirname, perm)
+    if not ok then
+        log.vimwarn("Failed making directory: ", err)
         return false
+    else
+        log.info("Directory made succesfully ", dirname)
+        return true
     end
 end
 
 -- Check if a dir exist, if not, try to making it.
 M.check_dir = function(dirname)
     log.trace("Checking dir", dirname)
-    if dirname == "" then return true end
-    if not M.path_exist(dirname) then
+    local stat = vim.loop.fs_stat(dirname)
+    if not stat then
         log.info(dirname .. " not found, making it")
-        if M.mkdir(dirname) then return true
-        else return false end
-    else
-        return true
+        if M.mkdir(dirname) then
+            return true
+        end
     end
+    ---@diagnostic disable-next-line: need-check-nil
+    if stat.type ~= "directory" then
+        log.vimwarn(dirname .. " is not a directory")
+        return false
+    end
+    return true
 end
 
--- Write especified content into a file.
+-- Write specified content into a file.
 M.writeFile = function(filename, lines)
-    log.trace("Writing ", filename)
-    local file = io.open(filename, "a+")
-    io.output(file)
-    for _, line in ipairs(lines) do
-        io.write(line .. "\n")
+    log.trace("Writing ", filename, lines)
+    local fd, err = vim.loop.fs_open(filename, "a+", 420)
+    if not fd then
+        log.vimerror("Failed to open file " .. filename .. ": " .. err)
+        return
     end
-    io.close(file)
+    vim.loop.fs_write(fd, table.concat(lines, "\n") .. "\n")
+    vim.loop.fs_close(fd)
 end
 
 -- Export ltex data depends on the type and lang especified.
 M.exportFile = function(type, lang, lines)
     log.trace("Exporting ", type, lang, lines)
     local filename = M.joinPath(type, lang)
-    if M.check_dir(path) then
+    if M.check_dir(M.path()) then
         M.writeFile(filename, lines)
     else
         log.vimwarn("Fail export " .. filename)
@@ -81,13 +83,18 @@ end
 -- Return the content of a required file
 M.readFile = function(filename)
     log.trace("Reading ", filename)
-    local lines, file = {}, io.open(filename, "r")
-    io.input(file)
-    for line in io.lines(filename) do
-        lines[#lines + 1] = line
+    local fd, err = vim.loop.fs_open(filename, "r", 420)
+    if not fd then
+        log.vimerror("Failed to open file " .. filename .. ": " .. err)
+        return {}
     end
-    io.close(file)
-    return lines
+    local stat = vim.loop.fs_fstat(fd)
+    if not stat then
+        return {}
+    end
+    local data = vim.loop.fs_read(fd, stat.size)
+    vim.loop.fs_close(fd)
+    return data and vim.split(data, "\n", { trimempty = true }) or {}
 end
 
 -- Return the content of a required type and lang.
