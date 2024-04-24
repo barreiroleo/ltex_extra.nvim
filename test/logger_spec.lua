ce23beafdf9059e76b8d58370a014f5a821c16df
LeoBarreiro <a.leo.barreiro@gmail.com>
2024-04-24 04:39:55 -0300

    refactor: Opts module now provides legacy and tests otps

--- test/logger_spec.lua
+++ test/logger_spec.lua
@@ -11,7 +11,7 @@ end
 
 describe("Logger: Builders and use", function()
     before_each(function()
-        -- vim.schedule_wrap(P("Cleaning"))
+        require("ltex_extra").__debug_reset()
         require("ltex_extra.utils.log"):__debug_reset()
     end)
 
@@ -23,6 +23,13 @@ describe("Logger: Builders and use", function()
 
     it("Vim logger builder", function()
         require("ltex_extra.utils.log"):new({ logLevel = "warn", usePlenary = false })
+        local logger = require("ltex_extra.utils.log").log
+        assert(doLog(logger) == true, "doLog function didn't finish its execution")
+    end)
+
+    it("Get logger from ltex extra", function()
+        local opts = require("ltex_extra.opts").tests_opts
+        require("ltex_extra").setup(opts)
         local logger = require("ltex_extra.utils.log").log
         assert(doLog(logger) == true, "doLog function didn't finish its execution")
     end)
